bb2442a8d588128b120108cbdce16d077d59a610
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-01-28 14:23:09 +0300

    made it possible to save values

--- main.js
+++ main.js
@@ -38,7 +38,24 @@ function addItem(e) {
     itemsList.prepend(newElement);
     //Очистим поле добавления новой задачи
     newItemInput.value = "";
+
+    //Сохранение в localStorage
+    todoList.push(newItemText);
+    localStorage.setItem('todo', JSON.stringify(todoList));
 }
+
+let todoList = [];
+
+let jsonString;
+//Проверяем есть ли задачи в LS или нет
+if (!localStorage.getItem('todo')) {
+    todoList = [];
+} else {
+    jsonString = localStorage.getItem('todo');
+    todoList = JSON.parse(jsonString);
+}
+console.log(todoList);
+
 
 //Удаление элемента
 itemsList.addEventListener('click', removeItem);
@@ -48,10 +65,38 @@ function removeItem(e) {
     if (e.target.getAttribute('data-action') == 'delete') {
         if (confirm('Удалить задачу')) {
             //Удаление родителя элемента
+            jsonString = localStorage.getItem('todo');
+            todoList = JSON.parse(jsonString);
+            todoList = todoList.filter(todo => todo !== e.target.parentNode.firstChild.textContent)
+            localStorage.setItem('todo', JSON.stringify(todoList))
             e.target.parentNode.remove();
+
         }
     }
 }
+
+//Вывод полученных данных из LS на страницу
+
+todoList.forEach((item) => {
+    let newElement = document.createElement('li');
+    newElement.className = 'list-group-item';
+    //Добавим текст в новый элемент
+    let newTextNode = document.createTextNode(item);
+    newElement.appendChild(newTextNode);
+    //Создаем кнопку
+    let deleteBtn = document.createElement('button');
+    //Добавляем текст в кнопку
+    deleteBtn.appendChild(document.createTextNode('удалить'));
+    //Добавляем Css класс в кнопку
+    deleteBtn.className = 'btn btn-light btn-sm float-right';
+    //Добавляем data-аттрибут
+    deleteBtn.dataset.action = 'delete';
+    //Помещаем кнопку внутрь тега li
+    newElement.appendChild(deleteBtn);
+    //Добавление на страницу задачи при вводе в инпут
+    itemsList.prepend(newElement);
+})
+
 
 //Фильтрация - прослушка ввода
 filter.addEventListener('keyup', filterItems);
